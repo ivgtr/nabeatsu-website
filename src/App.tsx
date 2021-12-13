c3da167e2f7b2dd80fd3614bcbeeeeb6fa277078
Yuto Watanabe <yuto.w51942@gmail.com>
2021-12-13 20:58:19 +0900

    wwwをわらわらわらと読むように修正

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export const App = () => {
   const speak = (text: string) => {
     return new Promise((resolve, _reject) => {
       speechSynthesis.cancel()
-      const options = new SpeechSynthesisUtterance(text)
+      const options = new SpeechSynthesisUtterance(text.replace('www', 'わらわらわら'))
       options.lang = 'ja-JP'
       options.onend = options.onerror = () => resolve(speechSynthesis.cancel())
       speechSynthesis.speak(options)
